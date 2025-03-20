fd851cf048cb3ee4c604862e35ef76d3823e068a
Kenzie1234567 <kenziegorre@gmail.com>
2025-03-20 19:19:47 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+let inputuser = document.getElementById('inputuser');
+let inputpass = document.getElementById('inputpass');
+let btnlogin = document.getElementById('btnlogin');
+
+btnlogin.addEventListener("click", function(e) {
+	
+	  e.preventDefault()
+	  
+	  if (inputuser.value == "Kenzie") {
+		  
+		  if (inputpass.value == "1234"){
+			  
+			  location.href = "main.html";
+			  
+		  } else {
+			  alert ("Invalid Password");
+		  }
+    } else { 
+	     alert ("Invalid Username");
+	}
+	
+});
